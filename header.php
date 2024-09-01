07152d3e49a1f12e2468d869473d9bde9ed08ff2
RomaP28 <posokhovrom@gmail.com>
2024-09-01 20:31:29 +0300

    mobile

--- header.php
+++ header.php
@@ -10,10 +10,10 @@
     <?php wp_head(); ?>
 
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/src/app/img/fav/apple-touch-icon.png'; ?>"
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/src/app/img/favicon-32x32.png'; ?>"
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/src/app/img/favicon-16x16.png'; ?>"
-    <link rel="manifest" href="<?php echo get_template_directory_uri() . '/src/app/img/site.webmanifest'; ?>"
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/src/app/img/safari-pinned-tab.svg'; ?>" color="#5bbad5">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/src/app/img/fav/favicon-32x32.png'; ?>"
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/src/app/img/fav/favicon-16x16.png'; ?>"
+    <link rel="manifest" href="<?php echo get_template_directory_uri() . '/src/app/img/fav/site.webmanifest'; ?>"
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/src/app/img/fav/safari-pinned-tab.svg'; ?>" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
--- inc/init.php
+++ inc/init.php
@@ -6,6 +6,18 @@ function p($arr) {
   echo '</pre>';
 }
 
+function smartwp_remove_wp_block_library_css(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+  wp_dequeue_style( 'global-styles' );
+  wp_dequeue_style( 'classic-theme-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+remove_action( 'wp_head', 'wp_generator' );
+
+
+
 register_nav_menus(
   array(
     'header_menu' => esc_html__( 'Header Menu', 'foodieland' ),
