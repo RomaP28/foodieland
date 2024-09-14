54636bbccffb95cc0c1addc17b1aa64384329887
RomaP28 <posokhovrom@gmail.com>
2024-09-14 16:57:46 +0300

    recipe

--- single-recipe.php
+++ single-recipe.php
@@ -63,7 +63,45 @@ get_header();
             </div>
         </div>
     </section>
-
+    <section class="info-section">
+        <div class="container">
+            <div class="video">
+                <img class="play" src="<?php echo get_template_directory_uri() . '/src/app/img/play.svg'; ?>" alt="play">
+                <img class="preview" src="<?php echo get_template_directory_uri() . '/src/app/img/preview.webp'; ?>" alt="preview">
+                <video controls muted playsinline autoplay>
+                    <source src="<?php echo get_template_directory_uri() . '/src/app/img/video.mp4'; ?>" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="nutrition">
+                <h3>Nutrition Information</h3>
+                <div class="row">
+                    <p class="name">Calories</p>
+                    <p class="val">219.9 kcal</p>
+                </div>
+                <div class="row">
+                    <p class="name">Total Fat</p>
+                    <p class="val">10.7 g</p>
+                </div>
+                <div class="row">
+                    <p class="name">Protein</p>
+                    <p class="val">7.9 g</p>
+                </div>
+                <div class="row">
+                    <p class="name">Carbohydrate</p>
+                    <p class="val">22.3 g</p>
+                </div>
+                <div class="row">
+                    <p class="name">Cholesterol</p>
+                    <p class="val">37.4 mg</p>
+                </div>
+                <p class="note">
+                    adipiscing elit, sed do eiusmod tempor<br>
+                    incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        </div>
+    </section>
 
 </main>
 
